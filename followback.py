1b55e25acf962b03ee63d87d5c1b432c666e7791
winiboya <w>
2023-01-07 19:03:35 -0600

    switched to set subtraction for efficiency

--- followback.py
+++ followback.py
@@ -6,24 +6,25 @@ with open("followers.json", "r") as f:
 with open("following.json", "r") as f:
     following = json.load(f)
 
-# create empty lis
-followers_list = []
-not_following = []
+# create empty sets
+followers_set = set()
+following_set = set()
 
-# save a list of all followers
+# save a set of all followers
 for person in followers["relationships_followers"]:
     person = person["string_list_data"][0]["value"]
-    followers_list.append(person)
+    followers_set.add(person)
 
-# check in following for people who are not followers
+# save a set of all following
 for person in following["relationships_following"]:
     person = person["string_list_data"][0]["value"]
-    # add to a list of people who are not following back
-    if (person not in followers_list):
-        not_following.append(person)
+    following_set.add(person)
+
+# find the difference between the lists
+not_following = following_set.difference(followers_set)
 
 # print the users not following back
 print(str(len(not_following)) + " are not following you back.")
-not_following.sort()
+not_following = sorted(not_following)
 for username in not_following:
     print("@" + username)
